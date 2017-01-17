f479e0a332ee93425a1ccf08dc543bde4994789c
Vedaste Cyizere <revedaste@gmail.com>
2017-01-17 17:51:52 +0200

    brand app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
 class PagesController < ApplicationController
 
   def fortune
-    render "fortune.html"
   end 
+
+  def lottery_numbers
+    lottery_numbers = [rand(50...500)]
+    @lottery_number = lottery_numbers.sample
+    render "lottery_numbers.html" 
+  end 
+
+  def user_views 
+   render user_views
+
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get "/fortune", to 'pages#fortune'
+  get '/fortune', to: 'pages#fortune'
+  get '/lottery_numbers' , to: 'pages#lottery_numbers'
 end
